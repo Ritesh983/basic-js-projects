b0ec8b77e73fab8e388cd31cae1f25f1ab6d0169
Ritesh983 <ritesh20040214@gmail.com>
2024-12-01 02:16:36 +0530

    second commit

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -19,6 +19,14 @@ const winnerPatterns = [
     [2, 4, 6]
 ];
 
+const resetGame=()=>{
+    turn0=true;
+    count=0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+};
+
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
         console.log("box was clicked");
@@ -32,9 +40,55 @@ boxes.forEach((box) => {
         box.disabled = true;
         count++;
 
-        let isWinner = checkWinnner();
-        if (count === 9 && !iswinner) {
+        let isWinner = checkWinner();
+        if (count === 9 && !isWinner) {
             gameDraw();
         }
     });
 });
+
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+
+
+const gameDraw = () => {
+    msg.innerText = `Game was a Draw.`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
+
+  const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
+
+  const checkWinner = () => {
+    for (let pattern of winnerPatterns) {
+      let pos1Val = boxes[pattern[0]].innerText;
+      let pos2Val = boxes[pattern[1]].innerText;
+      let pos3Val = boxes[pattern[2]].innerText;
+  
+      if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if (pos1Val === pos2Val && pos2Val === pos3Val) {
+          showWinner(pos1Val);
+          return true;
+        }
+      }
+    }
+  };
+  
+  newBtn.addEventListener("click", resetGame);
+  resetBtn.addEventListener("click", resetGame);
